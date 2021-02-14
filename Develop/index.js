9b84a083f92f7215c57c996cec56cc32d36b97a9
Rachel-Reidenga <reidenga@gmail.com>
2021-02-14 12:16:19 -0600

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,85 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer")
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "user-name",
+            message: "What is your GitHub username?",
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+
+        {
+            type: "input",
+            name: "projectName",
+            message: "What is the name of your project?",
+        },
+
+        {
+            type: "input",
+            name: "description",
+            message: "Please write a short description of your project",
+        },
+
+        {
+            type: "input",
+            name: "usage",
+            message: "Please write a short description of what you project is used for",
+        },
+
+        {
+            type: "input",
+            name: "license",
+            message: "What kind of license should your project have?",
+        },
+
+        {
+            type: "input",
+            name: "command",
+            message: "What command should be run to instal dependencies?",
+        },
+
+        {
+            type: "input",
+            name: "test",
+            message: "What command should be run to run tests?",
+        },
+
+        {
+            type: "input",
+            name: "repo",
+            message: "What does the user need to know about using the repo?",
+        },
+
+        {
+            type: "input",
+            name: "contributing",
+            message: "What does the user need to know about contributing to the repo?",
+        },
+
+
+
+    ])
+    .then(answers => {
+        console.log(answer.user-name);
+        console.log(answer.email);
+        console.log(answer.projectName);
+        console.log(answer.description);
+        console.log(answer.usage);
+        console.log(answer.license);
+        console.log(answer.command);
+        console.log(answer.test);
+        console.log(answer.repo);
+        console.log(answer.contributing);
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `[license](https://img.shields.io/badge/license-${license}-blue)`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `[license](#license)`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,8 +16,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
 
+
+##Description
+${data.description}
+
+##Table of Contents
+[Installation](#Installation)
+[Usage](#usage)
+[Credits](#contributing)
+[License](#license)
+
+##Installation
+${data.command}
+
+##Usage
+${data.usage}
+
+##License
+${renderLicenseLink(data.license)}
+
+###Contributing
+${data.contributing}
+
+###Tests
+${data.test}
+
+###Repo
+${data.repo}
 `;
 }
 
